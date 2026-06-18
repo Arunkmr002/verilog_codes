89c4560d2af966454d6e8c2abf6d00043c42d1b5
Arunkmr002 <arunkumar456429@gmail.com>
2026-06-18 15:55:32 +0530

    Create ripple_carry_adder_4bit_tb.v

---
+++ intermediate_combinational/ripple_carry_adder/ripple_carry_adder_4bit_tb.v
@@ -0,0 +1,19 @@
+module ripple_carry_adder_4bit_tb;
+reg [3:0] a,b;
+reg cin;
+wire[3:0] s;
+wire cout;
+integer i;
+ripple_carry_adder_4bit dut (.a(a),.b(b),.cin(cin),.s(s),.cout(cout));
+initial begin
+$dumpfile("wave.vcd");
+$dumpvars(0,ripple_carry_adder_4bit_tb);
+$display("A B Cin | S Cout");
+$monitor("%b %b %b |%b %b",a,b,cin,s,cout);
+for (i=0;i<512;i=i+1) begin
+   {a,b,cin} = i;
+#10;
+end
+$finish;
+end
+endmodule
