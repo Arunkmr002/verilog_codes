f9bb0438a922ce689d7fec794468c7e0365725a7
Arunkmr002 <arunkumar456429@gmail.com>
2026-07-02 14:22:47 +0530

    Create sr_latch_nor_2.v

---
+++ latches_and_flipflop/latches/sr_latch/sr_latch_nor/sr_latch_nor_2.v
@@ -0,0 +1,19 @@
+module sr_latch (
+input s,r,
+output reg q,not_q);
+
+always@(*)begin 
+ 
+if (s && !r)
+    q = 1'b1;
+else if (!s && r)
+    q = 1'b0;
+else if (!s && !r)
+    q = q ;
+else 
+    q = 1'bx;
+ 
+not_q = ~q;
+
+end
+endmodule
