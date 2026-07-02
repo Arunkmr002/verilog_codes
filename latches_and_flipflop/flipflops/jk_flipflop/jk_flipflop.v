e5b8eaf434cfd504019fa340df93b600a3223b3a
Arunkmr002 <arunkumar456429@gmail.com>
2026-07-02 14:47:35 +0530

    Create jk_flipflop.v

---
+++ latches_and_flipflop/flipflops/jk_flipflop/jk_flipflop.v
@@ -0,0 +1,19 @@
+module jk_flipflop(
+input clk,j,k,
+output reg q,
+output not_q);
+
+always@(posedge clk)begin
+
+case ({j,k})
+      2'b00 : q <= q;
+      2'b01 : q <= 1'b0;
+      2'b10 : q <= 1'b1;
+      2'b11 : q <= ~q;
+    default : q <= q;
+endcase
+end
+
+assign not_q = ~q;
+
+endmodule
