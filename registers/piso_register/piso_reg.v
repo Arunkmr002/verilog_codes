051a3a1515b46fb1203af7329d8a269e9c8d8f25
Arunkmr002 <arunkumar456429@gmail.com>
2026-07-06 09:26:15 +0530

    Create piso_reg.v

---
+++ registers/piso_register/piso_reg.v
@@ -0,0 +1,28 @@
+module piso_reg(
+input clk,
+input rst,
+input load,
+input si,
+input [3:0]pi,
+output so);
+
+reg [3:0]q;
+
+always@(posedge clk) begin
+if(rst)
+      q <=4'b0000;
+    
+else if (load) 
+       q <= pi;
+
+else begin
+	q[3] <= si;
+	q[2] <= q[3];
+	q[1] <= q[2];
+	q[0] <= q[1];
+end
+end
+
+assign so = q[0];
+
+endmodule
