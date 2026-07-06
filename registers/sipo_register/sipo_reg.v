05d02202ca25b0338e4466393dce645b17dc955b
Arunkmr002 <arunkumar456429@gmail.com>
2026-07-06 09:21:43 +0530

    Create sipo_reg.v

---
+++ registers/sipo_register/sipo_reg.v
@@ -0,0 +1,22 @@
+module sipo_reg(
+input clk,
+input rst,
+input si,
+output [3:0]po);
+
+reg [3:0] q;
+
+always@(posedge clk) begin
+if (rst)
+    q <= 4'b0000;
+else begin
+    q[3] <= si;
+    q[2] <= q[3];
+    q[1] <= q[2];
+    q[0] <= q[1];
+end
+end
+
+assign po = {q[3],q[2],q[1],q[0]};
+
+endmodule
