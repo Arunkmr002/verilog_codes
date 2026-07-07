783753e4c25a89b925345b816a267d75e2515aff
Arunkmr002 <arunkumar456429@gmail.com>
2026-07-07 12:24:30 +0530

    Create bcd_counter.v

---
+++ counters/bcd_counter/bcd_counter.v
@@ -0,0 +1,14 @@
+module bcd_counter(
+input clk,
+input rst,
+output reg [3:0]q);
+
+always@(posedge clk)begin
+if(rst)
+  q <= 4'b0000;
+else if (q == 4'b1001)
+  q <= 4'b0000;
+else
+  q <= q + 1'b1;
+end
+endmodule
