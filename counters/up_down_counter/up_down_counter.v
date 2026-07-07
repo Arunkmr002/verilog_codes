a8276b270fa5b5315b75bed4abcd6c0a9eb2b9c4
Arunkmr002 <arunkumar456429@gmail.com>
2026-07-07 12:15:49 +0530

    Create up_down_counter.v

---
+++ counters/up_down_counter/up_down_counter.v
@@ -0,0 +1,15 @@
+module up_down_counter(
+input clk,
+input rst,
+input up_down,
+output reg [3:0]q);
+
+always@(posedge clk)begin
+if (rst)
+   q <= 4'b0000;
+else if (up_down)
+   q <= q+1'b1;
+else
+   q <= q-1'b1;
+end
+endmodule
