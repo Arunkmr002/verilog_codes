d1912cba9397026db08a07f3b9d9337ae8ecbe9d
Arunkmr002 <arunkumar456429@gmail.com>
2026-07-07 12:27:20 +0530

    Create synchronous_counter_tb.v

---
+++ counters/synchronous_counter/synchronous_counter_tb.v
@@ -0,0 +1,28 @@
+module sync_counter_tb;
+reg clk;
+reg rst;
+wire [3:0]q;
+
+sync_counter dut (.clk(clk),.rst(rst),.q(q));
+initial begin
+clk=0;
+forever #5 clk = ~clk;
+end
+
+initial begin
+$dumpfile("wave.vcd");
+$dumpvars(0,sync_counter_tb);
+$monitor("time=%0t clk=%b rst=%b |q=%b",$time,clk,rst,q);
+
+#5;
+rst=1;
+#10;
+
+rst=0;
+#10;
+
+#200;
+
+$finish;
+end
+endmodule
