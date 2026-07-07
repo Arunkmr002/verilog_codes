bea99bc3ef967ff488bb1d3e30630552c132baf2
Arunkmr002 <arunkumar456429@gmail.com>
2026-07-07 12:29:14 +0530

    Create asynchronous_counter_tb.v

---
+++ counters/asynchronous_counter/asynchronous_counter_tb.v
@@ -0,0 +1,34 @@
+module async_counter_tb;
+reg clk;
+reg rst;
+wire [3:0]q;
+
+async_counter dut (.clk(clk),.rst(rst),.q(q));
+initial begin 
+clk=0;
+forever #5 clk = ~clk;
+end
+
+initial begin
+$dumpfile("wave.vcd");
+$dumpvars(0,async_counter_tb);
+$monitor("time=%0t clk=%b rst=%b |q=%b",$time,clk,rst,q);
+
+#5;
+rst=1;
+#10;
+
+rst=0;
+#10;
+
+#180;
+
+rst=1;
+#10;
+
+rst=0;
+#10;
+
+$finish;
+end
+endmodule
