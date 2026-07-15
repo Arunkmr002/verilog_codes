020ade13fe8f8361d00ce312f9108032ee98dd7b
Arunkmr002 <arunkumar456429@gmail.com>
2026-07-15 17:49:31 +0530

    Create sequence_detector_101_non_overlap_tb.v

---
+++ finite_state_machine/sequence_detector_101/sequence_detector_101_non_overlap_tb.v
@@ -0,0 +1,48 @@
+module sequence_detector_101_tb;
+reg clk;
+reg rst;
+reg in;
+wire out;
+wire [1:0]state_dbg;
+
+sequence_detector_101 dut(.clk(clk),.rst(rst),.in(in),.out(out),.state_dbg(state_dbg));
+initial begin
+clk=0;
+forever #5 clk = ~clk;
+end
+
+initial begin
+$dumpfile("wave.vcd");
+$dumpvars(0,sequence_detector_101_tb);
+$monitor("time=%0t clk=%b rst=%b in=%b |state=%b out=%b",$time,clk,rst,in,state_dbg,out);
+
+#5;
+rst=1;
+in =0;
+#10;
+
+rst=0;
+in =0;
+#10;
+
+in=1; #10;
+rst=1;#5;
+
+rst=0;#10;
+
+in=0; #10;
+in=1; #10;
+in=0; #10;
+in=1; #10;
+in=0; #10;
+in=1; #10;
+in=0; #10;
+in=1; #10;
+in=0; #10;
+
+rst=1; #10;
+rst=0; #10;
+
+$finish;
+end
+endmodule
