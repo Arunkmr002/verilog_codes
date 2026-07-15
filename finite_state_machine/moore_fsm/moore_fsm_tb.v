316ecd3996a537ae1b8a900d62240c5adbdad62c
Arunkmr002 <arunkumar456429@gmail.com>
2026-07-15 17:43:05 +0530

    Create moore_fsm_tb.v

---
+++ finite_state_machine/moore_fsm/moore_fsm_tb.v
@@ -0,0 +1,54 @@
+module moore_fsm_tb;
+reg clk;
+reg rst;
+reg in;
+wire out;
+
+wire [1:0] state_dbg;
+
+moore_fsm dut (.clk(clk),.rst(rst),.in(in),.out(out),.state_dbg(state_dbg));
+initial begin
+clk=0;
+forever #5 clk = ~clk;
+end
+
+initial begin
+$dumpfile("wave.vcd");
+$dumpvars(0,moore_fsm_tb);
+$monitor("time=%0t clk=%b rst=%b in=%b |state=%b | out=%b",$time,clk,rst,in,state_dbg,out);
+
+#5;
+rst=1;
+in =0;
+#10;
+
+rst=0;
+in =0;
+#10;
+
+in=1;
+#10;
+
+in=0;
+#10;
+
+in=1;
+#10;
+
+in=0;
+#10;
+
+in=1;
+#10;
+
+in=0;
+#10;
+
+in=1;
+#10;
+
+#20;
+
+$finish;
+end
+endmodule
